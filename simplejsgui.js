bc34b9507bee956b04e246b75602540347d357fc
JureVidrih <jurevidrih@gmail.com>
2018-05-29 11:45:50 +0200

    I've accidentally changed the background image to a larger file.

--- simplejsgui.js
+++ simplejsgui.js
@@ -2,7 +2,7 @@ SimpleJSGui.init();
 
 SimpleJSGui.getDesktop().shouldBackgroundCoverWholeViewport(true);
 SimpleJSGui.getDesktop().changeBackgroundColor("#ffffff");
-SimpleJSGui.getDesktop().changeBackgroundImage("backgrounds/markus-spiske-507983-unsplash.jpg");
+SimpleJSGui.getDesktop().changeBackgroundImage("backgrounds/pablo-acevedo-359265-unsplash.png");
 
 var myPanel = new Panel();
 
